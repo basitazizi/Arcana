bccc9a83373ee0e77d83dec3b1b06b4a50c78a01
basitazizi <112999295+basitazizi@users.noreply.github.com>
2025-08-27 22:01:33 -0700

    update and push all

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js  (DO NOT COMMIT)
+export const firebaseConfig = {
+    apiKey: "AIzaSyBPQMVulxP9q5F261XmypMiW8W9VL-5rmA",
+    authDomain: "arcana-4b1d0.firebaseapp.com",
+    projectId: "arcana-4b1d0",
+    storageBucket: "arcana-4b1d0.firebasestorage.app",
+    messagingSenderId: "409077089199",
+    appId: "1:409077089199:web:4aea191c8587bd4d95e55a",
+    measurementId: "G-JQLY00M51P"
+};
